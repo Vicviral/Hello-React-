7d5c1f91ac4ad9ddcb8ab54dfdb41cbae9df91f3
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-10-07 17:41:50 +0100

    Learn about States/List/ScrollView/Spread Operator

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  function getUserGoalInput(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addUserGoal() {
+    setGoals((previousGoals) => [...previousGoals, enteredGoalText]);
+    console.log(setGoals);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.goalsInputContainer}>
-        <TextInput style={styles.primaryTextInput} />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.primaryTextInput}
+          onChangeText={getUserGoalInput}
+        />
+        <Button title="Add Goal" color="#5e0acc" onPress={addUserGoal} />
       </View>
 
       <View style={styles.goalsContainer}>
-        <Text>List of Goals...</Text>
+        <ScrollView>
+          {goals.map((goal) => (
+            <View key={goal} style={styles.goalItem}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -22,16 +51,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
-  goalsInputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    borderBottomColor: "#cccccc",
-    borderBottomWidth: 1,
-    marginBottom: 20,
-    flex: 1,
-  },
-
   primaryTextInput: {
     borderColor: "#cccccc",
     borderWidth: 1,
@@ -39,7 +58,29 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8,
   },
 
+  goalsInputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "#cccccc",
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    flex: 1,
+  },
+
   goalsContainer: {
     flex: 9,
+  },
+
+  goalItem: {
+    marginTop: 8,
+    padding: 8,
+    borderBottomColor: "#5e0acc",
+    borderBottomWidth: 1,
+  },
+
+  goalText: {
+    color: "black",
   },
 });
